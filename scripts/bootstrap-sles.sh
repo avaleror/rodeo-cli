83ae65e8e2e85ffe8bfb965bf0321f5ff92c8368
Andres Valero <andres.valero@suse.com>
2026-06-13 10:55:52 +0200

    bootstrap: add --ref flag to set RODEO_REF

--- scripts/bootstrap-sles.sh
+++ scripts/bootstrap-sles.sh
@@ -70,6 +70,7 @@ while [[ $# -gt 0 ]]; do
   case $1 in
     --force) FORCE=true; shift ;;
     --lab-dir) LAB_DIR="$2"; shift 2 ;;
+    --ref) RODEO_REF="$2"; shift 2 ;;
     *) echo "Unknown option $1"; exit 1 ;;
   esac
 done
